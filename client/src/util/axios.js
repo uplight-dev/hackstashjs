049069ef231fd706a42d1c8903349093e86fb191
Dev 4Light <dev4light@gmail.com>
2020-09-22 20:08:38 +0200

    making debugging work

--- client/src/util/axios.js
+++ client/src/util/axios.js
@@ -2,5 +2,6 @@ import process from 'react';
 
 var axios = require('axios');
 
-console.log('REACT_BE_URL='+process.env.REACT_APP_BE_URL)
-axios.defaults.baseURL = process.env.REACT_APP_BE_URL;
+//console.log('REACT_BE_URL='+process.env.REACT_APP_BE_URL)
+//axios.defaults.baseURL = process.env.REACT_APP_BE_URL;
+axios.defaults.baseURL = 'http://localhost:3000'
